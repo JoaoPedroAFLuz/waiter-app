43c61b5e341699fd6930238f22d6ef81c0899e72
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-11-15 20:04:10 -0300

    feat: create header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  background: #d73035;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 198px;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 1216px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .page-details {
+    h1 {
+      color: #ffffff;
+      font-size: 32px;
+      margin-bottom: 6px;
+    }
+
+    h2 {
+      color: #ffffff;
+      font-size: 16px;
+      font-weight: 400;
+      opacity: 0.9;
+    }
+  }
+`;
