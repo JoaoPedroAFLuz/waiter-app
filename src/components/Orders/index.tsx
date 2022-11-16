e26ed8e6b6767f220d0a2b118c76e990e050cfe7
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-11-15 21:04:10 -0300

    feat: create OrdersBoard

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,67 +1,41 @@
-import { Container, Board, OrdersContainer } from './styles';
+import { Order } from '../../types/Order';
+import { OrdersBoard } from '../OrdersBoard';
+import { Container } from './styles';
+
+const order: Order[] = [
+  {
+    _id: '6372ed6ec7d4baf519bf74ee',
+    table: '1',
+    status: 'WAITING',
+    products: [
+      {
+        product: {
+          name: 'Pizza quatro queijos',
+          imagePath: '1668476096782-quatro-queijos.png',
+          price: 55,
+        },
+        quantity: 1,
+        _id: '6372ed6ec7d4baf519bf74ef',
+      },
+      {
+        product: {
+          name: 'Coca cola',
+          imagePath: '1668476032094-coca-cola.png',
+          price: 7.5,
+        },
+        quantity: 2,
+        _id: '6372ed6ec7d4baf519bf74f0',
+      },
+    ],
+  },
+];
 
 export function Orders() {
   return (
     <Container>
-      <Board>
-        <header>
-          <span>⌚</span>
-          <strong>Fila de espera</strong>
-          <span>(1)</span>
-        </header>
-
-        <OrdersContainer>
-          <button type="button">
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-
-          <button type="button">
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-        </OrdersContainer>
-      </Board>
-
-      <Board>
-        <header>
-          <span>⌚</span>
-          <strong>Fila de espera</strong>
-          <span>(1)</span>
-        </header>
-
-        <OrdersContainer>
-          <button type="button">
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-
-          <button type="button">
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-        </OrdersContainer>
-      </Board>
-
-      <Board>
-        <header>
-          <span>⌚</span>
-          <strong>Fila de espera</strong>
-          <span>(1)</span>
-        </header>
-
-        <OrdersContainer>
-          <button type="button">
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-
-          <button type="button">
-            <strong>Mesa 2</strong>
-            <span>2 itens</span>
-          </button>
-        </OrdersContainer>
-      </Board>
+      <OrdersBoard icon="⌚" title="Fila de espera." orders={order} />
+      <OrdersBoard icon="👨🏽‍🍳" title="Em preparação." orders={[]} />
+      <OrdersBoard icon="✅" title="Pronto!" orders={[]} />
     </Container>
   );
 }
---
+++ src/components/OrdersBoard/styles.ts
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+
+export const Board = styled.div`
+  padding: 16px;
+  border: 1px solid rgba(204, 204, 204, 0.4);
+  border-radius: 16px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+
+  header {
+    padding: 8px;
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+`;
+
+export const OrdersContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 24px;
+
+  button {
+    width: 100%;
+    background: #ffffff;
+    border: 1px solid rgba(204, 204, 204, 0.4);
+    height: 128px;
+    border-radius: 8px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 4px;
+
+    strong {
+      font-weight: 500;
+    }
+
+    span {
+      font-size: 14px;
+      color: #666666
+    }
+
+    & + button {
+      margin-top: 24px;
+    }
+  }
+`;
