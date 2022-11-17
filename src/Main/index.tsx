b70c142ef5efee5a438d445afa998b946eff79ac
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-11-16 23:10:22 -0300

    feat: create Table Modal Component

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -1,7 +1,10 @@
+import { useState } from 'react';
+
 import { Button } from '../components/Button';
 import { Categories } from '../components/Categories';
 import { Header } from '../components/Hearder';
 import { Menu } from '../components/Menu';
+import { TableModal } from '../components/TableModal';
 import {
   Container,
   CategoriesContainer,
@@ -11,6 +14,13 @@ import {
 } from './styles';
 
 export function Main() {
+  const [isTableModalVisible, setIsTableModalVisible] = useState(false);
+  const [selectedTable, setSelectedTable] = useState('');
+
+  function handleSaveTable(table: string) {
+    setSelectedTable(table);
+  }
+
   return (
     <>
       <Container>
@@ -25,9 +35,19 @@ export function Main() {
 
       <Footer>
         <FooterContainer>
-          <Button onPress={() => alert('Novo Pedido')}>Novo Pedido</Button>
+          {!selectedTable && (
+            <Button onPress={() => setIsTableModalVisible(true)}>
+              Novo Pedido
+            </Button>
+          )}
         </FooterContainer>
       </Footer>
+
+      <TableModal
+        visible={isTableModalVisible}
+        onClose={() => setIsTableModalVisible(false)}
+        onSave={handleSaveTable}
+      />
     </>
   );
 }
